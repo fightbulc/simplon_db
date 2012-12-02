ef17e6dee432ee0452044c85bd929e68955a2817
Tino Ehrich <ehrich@efides.com>
2012-12-02 15:10:44 +0100

    redis testing

--- test/test-redis.php
+++ test/test-redis.php
@@ -2,19 +2,277 @@
 
   require __DIR__ . '/../vendor/autoload.php';
 
-  echo '<h1>Redis</h1>';
-
-  $redis = \Simplon\Db\DbInstance::Redis('localhost', 0);
-  $redisManager = new \Simplon\Db\RedisManager($redis);
+  console('======================================');
+  console('REDIS (c-client phpiredis) testing shizzle');
+  console('======================================');
 
   // ############################################
 
-  echo '<h1>Strings</h1>';
+  $redis = \Simplon\Db\DbInstance::Redis('localhost', 1);
+  $redisManager = new \Simplon\Db\RedisManager($redis);
 
-  echo '<h3>Set</h3>';
-  $response = $redisManager->getRedisInstance()->stringSetMulti(['foobar' => 'hello', 'hello' => 'world']);
-  var_dump($response);
+  $redisManager
+    ->getRedisInstance()
+    ->dbFlush(TRUE);
 
-  echo '<h3>get</h3>';
-  $response = $redisManager->getRedisInstance()->stringGetData('foobar');
-  var_dump($response);
+  $runs = 3;
+  $sets = 1000;
+
+  // ############################################
+
+  console('+ Writing <strings> WITHOUT pipeline (runs=' . $runs . ', sets=' . $sets . ')');
+  $summary = 0;
+
+  for($t = 1; $t <= $runs; $t ++)
+  {
+    $start = microtime(TRUE);
+
+    for($i = 0; $i < $sets; $i ++)
+    {
+      $response = $redisManager
+        ->getRedisInstance()
+        ->stringSet('string:pipe:OFF:' . $t . '_' . $i, 'hello world ' . $i);
+    }
+
+    $finish = microtime(TRUE);
+    $timeTaken = $finish - $start;
+    $summary += $timeTaken;
+
+    console('time taken: ' . $timeTaken . ' seconds');
+  }
+
+  $summaryPipeOff = ($summary / $runs);
+
+  console('avg. time taken: ' . $summaryPipeOff . ' seconds');
+  console('--------------------------------------');
+
+  // ++++++++++++++++++++++++++++++++++++++++++++
+
+  console('+ Writing <strings> WITH pipeline (runs=' . $runs . ', sets=' . $sets . ')');
+  $summary = 0;
+
+  for($t = 1; $t <= $runs; $t ++)
+  {
+    $start = microtime(TRUE);
+    $pairs = [];
+
+    for($i = 0; $i < $sets; $i ++)
+    {
+      $pairs['string:pipe:ON:' . $t . ':' . $i] = 'hello world ' . $i;
+    }
+
+    $response = $redisManager
+      ->getRedisInstance()
+      ->stringSetMulti($pairs);
+
+    $finish = microtime(TRUE);
+    $timeTaken = $finish - $start;
+    $summary += $timeTaken;
+
+    console('time taken: ' . $timeTaken . ' seconds');
+  }
+
+  $summaryPipeOn = ($summary / $runs);
+
+  console('avg. time taken: ' . $summaryPipeOn . ' seconds');
+  console('--------------------------------------');
+  console('Pipeline = ' . ($summaryPipeOff / $summaryPipeOn) . 'x faster');
+  console('');
+  console('');
+
+  // ############################################
+
+  console('+ Reading <strings> WITHOUT pipeline (runs=' . $runs . ', sets=' . $sets . ')');
+  $summary = 0;
+
+  for($t = 1; $t <= $runs; $t ++)
+  {
+    $start = microtime(TRUE);
+
+    for($i = 0; $i < $sets; $i ++)
+    {
+      $response = $redisManager
+        ->getRedisInstance()
+        ->stringGetData('string:pipe:OFF:' . $t . ':' . $i);
+    }
+
+    $finish = microtime(TRUE);
+    $timeTaken = $finish - $start;
+    $summary += $timeTaken;
+
+    console('time taken: ' . $timeTaken . ' seconds');
+  }
+
+  $summaryPipeOff = ($summary / $runs);
+
+  console('avg. time taken: ' . $summaryPipeOff . ' seconds');
+  console('--------------------------------------');
+
+  // ++++++++++++++++++++++++++++++++++++++++++++
+
+  console('+ Reading <strings> WITH pipeline (runs=' . $runs . ', sets=' . $sets . ')');
+  $summary = 0;
+
+  for($t = 1; $t <= $runs; $t ++)
+  {
+    $start = microtime(TRUE);
+    $pairs = [];
+
+    for($i = 0; $i < $sets; $i ++)
+    {
+      $pairs[] = 'string:pipe:ON:' . $t . ':' . $i;
+    }
+
+    $response = $redisManager
+      ->getRedisInstance()
+      ->stringGetDataMulti($pairs);
+
+    $finish = microtime(TRUE);
+    $timeTaken = $finish - $start;
+    $summary += $timeTaken;
+
+    console('time taken: ' . $timeTaken . ' seconds');
+  }
+
+  $summaryPipeOn = ($summary / $runs);
+
+  console('avg. time taken: ' . $summaryPipeOn . ' seconds');
+  console('--------------------------------------');
+  console('Pipeline = ' . ($summaryPipeOff / $summaryPipeOn) . 'x faster');
+  console('');
+  console('');
+
+  // ############################################
+
+  $sets = 10000; // friends lists
+
+  console('+ Writing <lists> WITHOUT pipe (runs=' . $runs . ', sets=' . $sets . ')');
+  $summary = 0;
+
+  for($t = 1; $t <= $runs; $t ++)
+  {
+    $start = microtime(TRUE);
+
+    for($i = 0; $i < $sets; $i ++)
+    {
+      $response = $redisManager
+        ->getRedisInstance()
+        ->listUnshift('list:pipe:OFF:' . $t . ':' . $i, 'hello world ' . $i);
+    }
+
+    $finish = microtime(TRUE);
+    $timeTaken = $finish - $start;
+    $summary += $timeTaken;
+
+    console('time taken: ' . $timeTaken . ' seconds');
+  }
+
+  $summaryPipeOff = ($summary / $runs);
+
+  console('avg. time taken: ' . $summaryPipeOff . ' seconds');
+  console('--------------------------------------');
+
+  // ++++++++++++++++++++++++++++++++++++++++++++
+
+  console('+ Writing <lists> WITH pipe (runs=' . $runs . ', sets=' . $sets . ')');
+  $summary = 0;
+
+  for($t = 1; $t <= $runs; $t ++)
+  {
+    $start = microtime(TRUE);
+    $pairs = [];
+
+    for($i = 0; $i < $sets; $i ++)
+    {
+      $pairs['list:pipe:ON:' . $t . ':' . $i] = 'hello world ' . $i;
+    }
+
+    $response = $redisManager
+      ->getRedisInstance()
+      ->listMultiUnshift($pairs);
+
+    $finish = microtime(TRUE);
+    $timeTaken = $finish - $start;
+    $summary += $timeTaken;
+
+    console('time taken: ' . $timeTaken . ' seconds');
+  }
+
+  $summaryPipeOn = ($summary / $runs);
+
+  console('avg. time taken: ' . $summaryPipeOn . ' seconds');
+  console('--------------------------------------');
+  console('Pipe = ' . ($summaryPipeOff / $summaryPipeOn) . 'x faster');
+  console('');
+  console('');
+
+  // ############################################
+
+  $sets = 10000; // friends lists
+
+  console('+ Reading <lists> by range WITHOUT pipe (runs=' . $runs . ', sets=' . $sets . ')');
+  $summary = 0;
+
+  for($t = 1; $t <= $runs; $t ++)
+  {
+    $start = microtime(TRUE);
+
+    for($i = 0; $i < $sets; $i ++)
+    {
+      $response = $redisManager
+        ->getRedisInstance()
+        ->listGetDataByRange('list:pipe:OFF:' . $t . ':' . $i, 0, 500);
+    }
+
+    $finish = microtime(TRUE);
+    $timeTaken = $finish - $start;
+    $summary += $timeTaken;
+
+    console('time taken: ' . $timeTaken . ' seconds');
+  }
+
+  $summaryPipeOff = ($summary / $runs);
+
+  console('avg. time taken: ' . $summaryPipeOff . ' seconds');
+  console('--------------------------------------');
+
+  // ++++++++++++++++++++++++++++++++++++++++++++
+
+  console('+ Reading <lists> by range WITH pipe (runs=' . $runs . ', sets=' . $sets . ')');
+  $summary = 0;
+
+  for($t = 1; $t <= $runs; $t ++)
+  {
+    $start = microtime(TRUE);
+    $pairs = [];
+
+    for($i = 0; $i < $sets; $i ++)
+    {
+      $pairs[] = 'list:pipe:ON:' . $t . ':' . $i;
+    }
+
+    $response = $redisManager
+      ->getRedisInstance()
+      ->listGetDataByRangeMulti($pairs, 0, 50);
+
+    $finish = microtime(TRUE);
+    $timeTaken = $finish - $start;
+    $summary += $timeTaken;
+
+    console('time taken: ' . $timeTaken . ' seconds');
+  }
+
+  $summaryPipeOn = ($summary / $runs);
+
+  console('avg. time taken: ' . $summaryPipeOn . ' seconds');
+  console('--------------------------------------');
+  console('Pipe = ' . ($summaryPipeOff / $summaryPipeOn) . 'x faster');
+  console('');
+  console('');
+
+  // ############################################
+
+  function console($line)
+  {
+    echo "$line\n";
+  }
